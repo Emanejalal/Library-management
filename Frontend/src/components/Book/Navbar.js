b546cc97a20557c359f6d9405fa9b10846924117
Soufiane Benlahcen <soufiane.benlahcen18@gmail.com>
2024-05-30 04:31:34 +0100

    Navbar

--- Frontend/src/components/Book/Navbar.js
+++ Frontend/src/components/Book/Navbar.js
@@ -1,4 +1,3 @@
-// Navbar.js
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
@@ -12,27 +11,27 @@ function Navbar() {
   };
 
   return (
-    <nav className="bg-gray-100 p-4">
+    <nav className="bg-blue-900 p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <Link className="text-xl font-semibold" to="/">Library</Link>
+        <Link className="text-xl font-semibold text-white mr-4 hover:no-underline" to="/">Library</Link>
         <div className="flex space-x-4">
-          <Link className="text-gray-700 hover:text-gray-900" to="/Dashboard">Dashboard</Link>
-          <Link className="text-gray-700 hover:text-gray-900" to="/books">Books</Link>
-          <Link className="text-gray-700 hover:text-gray-900" to="/loans">Loans</Link>
-          {token && <Link className="text-gray-700 hover:text-gray-900" to="/users">Users</Link>}
+          <Link className="text-gray-300 hover:text-white hover:bg-white hover:px-2 hover:py-2 hover:border hover:border-transparent hover:rounded" to="/Dashboard">Dashboard</Link>
+          <Link className="text-gray-300 hover:text-white hover:bg-white hover:px-2 hover:py-2 hover:border hover:border-transparent hover:rounded" to="/books">Books</Link>
+          <Link className="text-gray-300 hover:text-white hover:bg-white hover:px-2 hover:py-2 hover:border hover:border-transparent hover:rounded" to="/loans">Loans</Link>
+          {token && <Link className="text-gray-300 hover:text-white hover:bg-white hover:px-2 hover:py-2 hover:border hover:border-transparent hover:rounded" to="/users">Users</Link>}
         </div>
         <div className="ml-auto">
           {token ? (
             <button
-              className="text-gray-700 hover:text-gray-900"
+              className="text-gray-300 hover:text-white"
               onClick={handleLogout}
             >
               Logout
             </button>
           ) : (
             <>
-              <Link className="text-gray-700 hover:text-gray-900" to="/login">Login</Link>
-              <Link className="text-gray-700 hover:text-gray-900" to="/signup">Register</Link>
+              <Link className="text-gray-300 hover:text-white hover:bg-white hover:px-2 hover:py-2 hover:border hover:border-transparent hover:rounded" to="/login">Login</Link>
+              <Link className="text-gray-300 hover:text-white hover:bg-white hover:px-2 hover:py-2 hover:border hover:border-transparent hover:rounded" to="/signup">Register</Link>
             </>
           )}
         </div>
