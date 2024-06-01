5e10806ad817082c26ef64ab3360b3ebb0cf69d1
Soufiane Benlahcen <soufiane.benlahcen18@gmail.com>
2024-06-01 03:35:25 +0100

    pre finals

--- Frontend/src/pages/About.js
+++ Frontend/src/pages/About.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import landingImage from '../images/lib.jpg'; // Assurez-vous d'avoir une image à ce chemin
-import user1Image from '../images/1.jpeg'; // Assurez-vous d'avoir une image à ce chemin
-import user2Image from '../images/2.png'; // Assurez-vous d'avoir une image à ce chemin
+import landingImage from '../images/lib.jpg'; // Ensure you have an image at this path
+import user1Image from '../images/1.jpeg'; // Ensure you have an image at this path
+import user4Image from '../images/4.jpg'; // Ensure you have an image at this path
 
 function About() {
   return (
@@ -11,14 +11,26 @@ function About() {
     >
       <form className="bg-white bg-opacity-50 p-8 m-5 w-3/5 h-3/5 rounded-lg backdrop-blur-md transition transform duration-300 hover:scale-105 hover:bg-opacity-60">
         <h2 className="text-center mb-8 text-4xl font-bold">About Us</h2>
-        <div className="flex justify-around">
+        <div className="flex justify-around mt-8"> {/* Added mt-8 to move the images down */}
           <div className="flex flex-col items-center">
-            <img src={user1Image} alt="User 1" className="w-48 h-48 rounded-full mb-4" />
+            <div className="w-48 h-48 mb-4">
+              <img 
+                src={user1Image} 
+                alt="User 1" 
+                className="w-full h-full object-cover rounded-full" 
+              />
+            </div>
             <p className="text-2xl font-semibold">Imane JALAL</p>
             <p className="text-xl">Big Data Analytics</p>
           </div>
           <div className="flex flex-col items-center">
-            <img src={user2Image} alt="User 2" className="w-48 h-48 rounded-full mb-4" />
+            <div className="w-48 h-48 mb-4">
+              <img 
+                src={user4Image} 
+                alt="User 2" 
+                className="w-full h-full object-cover rounded-full" 
+              />
+            </div>
             <p className="text-2xl font-semibold">Soufiane BENLAHCEN</p>
             <p className="text-xl">Big Data Analytics</p>
           </div>
